de1876666b3a827f6f94d3a2d711c6509e63df00
Jovita Tang <47584623+Brennil@users.noreply.github.com>
2025-07-31 17:58:31 +0800

    Update quizzer.py

--- quizzer.py
+++ quizzer.py
@@ -4,12 +4,15 @@ import random
 import gspread
 from google.oauth2 import service_account
 
+# === CONFIGURATION ===
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1JrlwY69DVRnOjFZvaE0CX_PLZNelw3_qEBu0WhgcHPQ/edit#gid=0"
+
 # === GOOGLE SHEETS AUTH ===
 def load_sheet():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive", 'https://www.googleapis.com/auth/spreadsheets']
     creds = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"],scopes = scope)
     client = gspread.authorize(creds)
-    spread = client.open("Computing Keywords (2025 Syllabus) (Quizzer)")
+    spread = client.open_by_url(SHEET_URL)
     print(spread)
     sheet = spread.worksheet("01")
     data = sheet.get_all_records()
