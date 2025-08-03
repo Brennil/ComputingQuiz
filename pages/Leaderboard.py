6203b41cac0c8378864b1a4bd1e8ca79033a3a77
Jovita Tang <47584623+Brennil@users.noreply.github.com>
2025-08-03 21:10:34 +0800

    Create Leaderboard.py

---
+++ pages/Leaderboard.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import random
+import gspread
+from datetime import datetime
+import pytz
+from google.oauth2 import service_account
+
+# === CONFIGURATION ===
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1JrlwY69DVRnOjFZvaE0CX_PLZNelw3_qEBu0WhgcHPQ/edit#gid=0"
+
+# === GOOGLE SHEETS AUTH ===
+def load_spread():
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive", 'https://www.googleapis.com/auth/spreadsheets']
+    creds = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"],scopes = scope)
+    client = gspread.authorize(creds)
+    spread = client.open_by_url(SHEET_URL)
+    return spread
+
+spread = load_spread()
+
+st.title("👑 Leaderboards")
+
+options = ["01", "02", "03"]
+chapter = st.selectbox("Choose a chapter:", options)
+go = st.button("Go!")
+
+if go:
+    sheet = spread.worksheet("Log"+chapter)
+    data = sheet.get_all_records()
+    df = pd.DataFrame(data)
+    top = {}
+    for i, row in df.iterrows():
+        if row["Names"] in top.keys() and float(row["Accuracy"]) > top[row["Names"]]:
+            top[row["Names"]] = float(row["Accuracy"])
+        elif not row["Names"] in top.keys():
+            top[row["Names"]] = float(row["Accuracy"])
+    print(top)
+          
+  
