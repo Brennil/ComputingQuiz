5c4fe9a2b8c67e3d9a9db9ae72173cdf6e8b3134
Jovita Tang <47584623+Brennil@users.noreply.github.com>
2025-08-11 17:58:37 +0800

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -65,12 +65,13 @@ else:
         records = pd.DataFrame(logdata)
         history = [0]*len(df)
         attempt_count = 0
+        st.write(df['Questions'])
         for i, row in records.iterrows():
             if row['Email'] == st.user.email:
                 attempt_count += 1
                 for x in range(4, len(row)):
                     if row.iloc[x] == "NA": history[x-4] += 0
-                    else: history[x-4] += row.iloc[x]
+                    else: history[x-4] += int(row.iloc[x] == df['Questions'][x-4]])
         
         if attempt_count > 0:
             history = list(history)
