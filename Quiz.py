88039d3122a928fbd01c22616791799f97463fa9
Jovita Tang <47584623+Brennil@users.noreply.github.com>
2025-08-12 17:37:13 +0800

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -18,6 +18,10 @@ def load_spread():
     return spread
 
 spread = load_spread()
+global chapter
+global sheet
+global data
+global df
 
 def login_screen():
     st.subheader("Please log in with your SST account to play.")
@@ -35,6 +39,20 @@ def main():
     options = ["01 Computer Architecture", "02-03 Data Representation; Logic Gates", "04 Programming", "05-08 Input Validation; Testing and Debugging; Algorithm Design; Software Engineering", "09 Spreadsheets", "10 Networking", "11 Security and Privacy", "12-14 Intellectual Property; Impact of Computing; Emerging Technologies"]
     chapter_sel = st.selectbox("Choose a chapter:", options)
     chapter = chapter_sel[:chapter_sel.find(" ")]
+    
+    go = st.button("Go!")
+
+    if not st.session_state.quiz_started:
+        if go:
+            st.session_state.quiz_started = True
+            st.session_state.questions = None  
+            st.rerun()
+    else:
+        sheet = spread.worksheet(chapter)
+        data = sheet.get_all_records()
+        df = pd.DataFrame(data)
+
+        quiz()
     
 def quiz():
     # === LOAD OR CREATE USERLOG ===
@@ -120,18 +138,5 @@ if not st.user.is_logged_in:
     login_screen()
 else:
     main()
-    go = st.button("Go!")
-
-    if not st.session_state.quiz_started:
-        if go:
-            st.session_state.quiz_started = True
-            st.session_state.questions = None  
-            st.rerun()
-    else:
-        sheet = spread.worksheet(chapter)
-        data = sheet.get_all_records()
-        df = pd.DataFrame(data)
-
-        quiz()
 
     st.button("Log out", on_click=st.logout)
