6fa500af3dc096ae484d747a785aa2fae830350a
Jovita Tang <47584623+Brennil@users.noreply.github.com>
2025-08-12 17:34:53 +0800

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -35,20 +35,7 @@ def main():
     options = ["01 Computer Architecture", "02-03 Data Representation; Logic Gates", "04 Programming", "05-08 Input Validation; Testing and Debugging; Algorithm Design; Software Engineering", "09 Spreadsheets", "10 Networking", "11 Security and Privacy", "12-14 Intellectual Property; Impact of Computing; Emerging Technologies"]
     chapter_sel = st.selectbox("Choose a chapter:", options)
     chapter = chapter_sel[:chapter_sel.find(" ")]
-    go = st.button("Go!")
-
-    if not st.session_state.quiz_started:
-        if go:
-            st.session_state.quiz_started = True
-            st.session_state.questions = None  
-            st.rerun()
-    else:
-        sheet = spread.worksheet(chapter)
-        data = sheet.get_all_records()
-        df = pd.DataFrame(data)
-
-        quiz()
-        
+    
 def quiz():
     # === LOAD OR CREATE USERLOG ===
     log = "Log"+chapter
@@ -133,5 +120,18 @@ if not st.user.is_logged_in:
     login_screen()
 else:
     main()
+    go = st.button("Go!")
+
+    if not st.session_state.quiz_started:
+        if go:
+            st.session_state.quiz_started = True
+            st.session_state.questions = None  
+            st.rerun()
+    else:
+        sheet = spread.worksheet(chapter)
+        data = sheet.get_all_records()
+        df = pd.DataFrame(data)
+
+        quiz()
 
     st.button("Log out", on_click=st.logout)
