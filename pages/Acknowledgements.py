4f8140ca5e746efeb79fe4781713498582df63f7
Jovita Tang <47584623+Brennil@users.noreply.github.com>
2025-08-12 18:26:24 +0800

    Create Acknowledgements.py

---
+++ pages/Acknowledgements.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("🙏 Acknowledgements")
+
+st.write("This quiz webapp was created by Ms Jovita Tang, with the support and debugging help from ChatGPT.")
+st.write("Thank you to Clifton Lim from S4-02, Class of 2025 for the beta test feedback.")
+st.write("If you have further suggestions or feedback, please contact Ms Jovita Tang. 💖")
+st.write("")
+st.write("Thank you for playing the quizzes. We hope that you have had fun.")
